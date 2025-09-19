5c1b8bdd4e5b3a0a057f540f4d95a99458a9d64d
shithin <shithinpm9846@gmail.com>
2025-09-19 08:24:28 +0530

    Timer

---
+++ AVR/timer_1/timer_1/main.c
@@ -0,0 +1,33 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+
+
+void delay_1s(void)
+{
+	unsigned int i;
+	unsigned int count=62500;
+	for(i=0; i<count; i++)
+	{
+	TCNT0=0;
+	TCCR0A=0;
+	TCCR0B|=(1<<CS00);
+	TIFR0|=(1<<TOV0); //making timer over flow flag
+		while (!(TIFR0 & (1<<TOV0)));//waiting timer overflow flag to set we get 16us delay
+}
+}
+
+int main(void)
+{
+	
+  DDRB |=(1<<DDB5); //PB5 as output
+	
+	while (1)
+	{
+		PORTB |=(1<<PORTB5);
+		delay_1s();
+		PORTB&=(1<<PORTB5);
+		delay_1s();
+		
+	}
+}
+

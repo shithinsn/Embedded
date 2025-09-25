6a72cab307525bf2cd5bd82e88e7efffc2b55dd4
shithin <shithinpm9846@gmail.com>
2025-09-26 00:01:10 +0530

    commit the files

---
+++ AVR/Fast_pwm_Timer0/Fast_pwm_Timer0/main.c
@@ -0,0 +1,18 @@
+/*
+ * Fast_pwm_Timer0.c
+ *
+ * Created: 24-09-2025 20:58:47
+ * Author : HP
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

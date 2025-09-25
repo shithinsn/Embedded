acad7c805a8ddd11bc496a8ec9e3948c1f20bb1a
shithin <shithinpm9846@gmail.com>
2025-09-26 00:59:10 +0530

    Comitting Fast_pwm_Timer0

--- AVR/Fast_pwm_Timer0/Fast_pwm_Timer0/main.c
+++ AVR/Fast_pwm_Timer0/Fast_pwm_Timer0/main.c
@@ -4,15 +4,25 @@
  * Created: 24-09-2025 20:58:47
  * Author : HP
  */ 
-
+#define F_CPU 16000000UL
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
+	DDRD|=(1<<PD6);
+		
     /* Replace with your application code */
     while (1) 
     {
+	 TCCR0A|=(1<<WGM01);
+	 TCCR0A|=(1<<WGM00);
+	 TCCR0A|=(1<<COM0A1);
+	 TCCR0B|=(1<<CS00);
+	 TCCR0B|=(1<<CS01);
+	 OCR0A=78.12;
+	 	
     }
 }
 
